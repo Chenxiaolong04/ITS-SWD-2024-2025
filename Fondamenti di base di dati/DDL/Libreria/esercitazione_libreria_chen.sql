d17b9d39ad8a3d64d5550bba6ab0ef02a1f7de5d
Chenxiaolong04 <chenxiaolong12345678@gmail.com>
2025-02-03 21:46:19 +0100

    Add files via upload

--- Fondamenti di base di dati/DDL/Libreria/esercitazione_libreria_chen.sql
+++ Fondamenti di base di dati/DDL/Libreria/esercitazione_libreria_chen.sql
@@ -18,7 +18,7 @@ from libro;
 -- 7. Ottenere la media delle pagine dei libri
 select avg(pagine)as media_pagine
 from libro;
--- 8. Trovare il libro più costoso--------------------------------------------------------------------------------------
+-- 8. Trovare il libro più costoso
 select titolo,prezzo
 from libro
 where prezzo=(
@@ -49,23 +49,39 @@ select *
 from libro, editore
 where libro.editore_id = editore.id and editore.id=3;
 -- 14. Contare il numero di libri per ogni editore
-
+SELECT editore.nome AS editore, COUNT(libro.titolo) AS numero_libri
+FROM libro
+INNER JOIN editore ON libro.editore_id = editore.id
+GROUP BY editore.nome
+ORDER BY numero_libri DESC;
 -- 15. Trovare i libri con più di 400 pagine pubblicati da un determinato editore
 select titolo,editore.nome
 from libro, editore
 where libro.pagine>400 and editore.id=1;
 -- 16. Trovare il numero di autori che hanno scritto almeno un libro
-
+SELECT COUNT(DISTINCT autore_id) AS numero_autori 
+FROM autore_libro;
 -- 17. Trovare gli autori che hanno scritto più di un libro
-
+SELECT autore.nome, autore.cognome, COUNT(autore_libro.libro_id) AS numero_libri
+FROM autore
+JOIN autore_libro ON autore.id = autore_libro.autore_id
+GROUP BY autore.id
+HAVING COUNT(autore_libro.libro_id) > 1;
 -- 18. Trovare gli autori che non hanno scritto alcun libro
-
+SELECT *
+FROM autore
+LEFT JOIN autore_libro ON autore.id = autore_libro.autore_id
+WHERE autore_libro.libro_id IS NULL;
 -- 19. Selezionare i libri con più di un autore
-
+SELECT libro.titolo, COUNT(autore_libro.autore_id) AS numero_autori
+FROM libro
+JOIN autore_libro ON libro.id = autore_libro.libro_id
+GROUP BY libro.id
+HAVING COUNT(autore_libro.autore_id) > 1;
+select *
+from autore_libro;
 -- 20. Contare il numero di libri per autore
-select count(nome)
-from autore
-group by nome;
+
 -- 21. Ottenere la somma totale del prezzo di tutti i libri
 select sum(prezzo) as prezzo_totale
 from libro;
@@ -74,11 +90,17 @@ select count(titolo) as libri
 from libro
 where prezzo between 10 and 20;
 -- 23. Trovare il prezzo medio dei libri per editore
+select avg(prezzo),editore_id
+from libro
+group by editore_id;
+
 
 -- 24. Trovare gli autori con più libri pubblicati da editori diversi
 
 -- 25. Trovare i libri con il prezzo più alto per ogni editore
-
+select max(prezzo),editore_id
+from libro
+group by editore_id;
 -- 26. Ottenere i 5 libri più costosi
 select prezzo
 from libro
