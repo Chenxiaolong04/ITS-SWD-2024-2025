c3e2e978af53abec9ff6d7232720df9a32a3bfc4
Chenxiaolong04 <chenxiaolong12345678@gmail.com>
2025-02-10 12:08:45 +0100

    Add files via upload

--- Fondamenti di programmazione (Python)/Lezione10_2_2025/es_10_2_2025.py
+++ Fondamenti di programmazione (Python)/Lezione10_2_2025/es_10_2_2025.py
@@ -1,1 +1,28 @@
-fruits_dict = {"mela": 5,"banana": 1,"arancia": 3}
+def find_fruit(key,fruits_dict):
+    try:
+        return key,fruits_dict[key]
+
+    except:
+        print("Errore: Frutto non trovato!") 
+#1 e 3
+fruits_dict = {"mela": 5, "banana": 1, "arancia": 3}
+frutta=input("Inserisci frutta: ")
+while True:
+    risultato=find_fruit(frutta,fruits_dict)
+    if risultato==None:
+        print("Valore inserito non valido inserire nuovamente: ")
+        frutta=input("Inserisci nuovamente frutta: ")
+    else:
+        risultato=find_fruit(frutta,fruits_dict)
+        print(risultato)
+        break
+#2
+fruits_tuple=tuple(fruits_dict.items())
+for key in fruits_tuple:
+    print(key)
+#4
+frutta=input("Inserisci frutta: ")
+if fruits_dict.get(frutta)==None:
+    print("Valore inserito non valido")
+else:
+    print(find_fruit(frutta,fruits_dict))
