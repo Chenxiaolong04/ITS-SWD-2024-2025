9ba31206127b2be05e4e3aebd2926acd7ba64fcf
Chenxiaolong04 <chenxiaolong12345678@gmail.com>
2025-04-15 14:29:40 +0200

    Delete Fondamenti di UX UI e HTML CSS/Esercitazione02 directory

--- Fondamenti di UX UI e HTML CSS/Esercitazione02/Esercitazione05/Parte01/script/main.js
+++
@@ -1,9 +0,0 @@
-let bool = true;
-setInterval(() => {
-    if (bool) {
-        document.documentElement.style.setProperty('--primary-color', '#FFFF00');
-    } else {
-        document.documentElement.style.setProperty('--primary-color', '#ffaadd');
-    }
-    bool = !bool;
-}, 100);
