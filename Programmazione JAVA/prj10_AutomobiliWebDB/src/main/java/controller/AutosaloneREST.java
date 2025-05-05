cd828ac5181652cf7a70fcd62c55623d37b520cc
Chenxiaolong04 <chenxiaolong12345678@gmail.com>
2025-05-05 13:02:56 +0200

    Update AutosaloneREST.java

--- Programmazione JAVA/prj10_AutomobiliWebDB/src/main/java/controller/AutosaloneREST.java
+++ Programmazione JAVA/prj10_AutomobiliWebDB/src/main/java/controller/AutosaloneREST.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.util.List;
 
 import org.json.JSONArray;
-import org.json.JSONObject;
 
 /**
  * Servlet implementation class AutosaloneREST
@@ -20,25 +19,12 @@ import org.json.JSONObject;
 @WebServlet("/api")
 public class AutosaloneREST extends HttpServlet {
 	
-	private AutomobileService service = new AutomobileService();
+	private AutomobileService service=new AutomobileService();
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		List<Automobile> automobili = service.getAutomobili();
-		
-		JSONArray array = new JSONArray();
-		for (Automobile auto : automobili) {
-			JSONObject obj = new JSONObject();
-			obj.put("id", auto.getId());
-			obj.put("marca", auto.getMarca());
-			obj.put("modello", auto.getModello());
-			obj.put("cilindrata", auto.getCilindrata());
-			obj.put("posti", auto.getPosti());
-			obj.put("prezzo", auto.getPrezzo());
-			array.put(obj);
-		}
-		
-		// Impostazione corretta del tipo di contenuto
+		JSONArray array=new JSONArray(service.getAutomobili());
 		response.setContentType("application/json");
 		response.getWriter().print(array.toString());
 	}
+
 }
